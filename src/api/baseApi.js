0ec1e285cc3dd59f8d42d2c2384d922220189b0d
PRINCE ROY <83387075+princeroy8606@users.noreply.github.com>
2024-08-13 11:03:31 +0530

    Update baseApi.js

--- src/api/baseApi.js
+++ src/api/baseApi.js
@@ -1,34 +1,36 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://csuite-production.up.railway.app" });
+const API = axios.create({
+  baseURL: "https://csuite-production.up.railway.app/api",
+});
 
 // user
-export const addnewUser = (userdata) => API.post("/api/user", userdata);
+export const addnewUser = (userdata) => API.post("/user", userdata);
 
-export const allUsers = () => API.get("/api/user",console.log("req->"));
+export const allUsers = () => API.get("/user", console.log("req->"));
 
 export const updateUser = (userdata, Id) =>
-  API.put(`/api/user/${Id}`, userdata);
+  API.put(`/user/${Id}`, userdata);
 
-export const deleteUser = (Id) => API.delete(`/api/user/${Id}`);
+export const deleteUser = (Id) => API.delete(`/user/${Id}`);
 
 // purchases
-export const allPurchases = () => API.get(`/api/payment`);
+export const allPurchases = () => API.get(`/payment`);
 
 // courses
-export const addnewCourse = (course) => API.post("/api/courseDetail/add", course);
+export const addnewCourse = (course) => API.post("/courseDetail/add", course);
 
-export const getAllCourse = () => API.get("/api/courseDetail");
+export const getAllCourse = () => API.get("/courseDetail");
 
 export const uploadVedio = (formdata) =>
-  API.post("/api/uploadtovimeo", formdata, {
+  API.post("/uploadtovimeo", formdata, {
     headers: { "Content-Type": "multipart/form-data" },
   });
 
 export const updateCourse = (updatedData) =>
-  API.put(`/api/courseDetail/edit/${updatedData?._id}`, updatedData);
+  API.put(`/courseDetail/edit/${updatedData?._id}`, updatedData);
 
 // test
-export const addnewTest = (test) => API.post("/api/tests", test);
-export const updateTest = (test) => API.put(`/api/tests/${test._id}`, test);
-export const getLessonTest = (testId) => API.get(`/api/tests/${testId}`);
+export const addnewTest = (test) => API.post("/tests", test);
+export const updateTest = (test) => API.put(`/tests/${test._id}`, test);
+export const getLessonTest = (testId) => API.get(`/tests/${testId}`);
